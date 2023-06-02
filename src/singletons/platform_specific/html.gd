eba08ef5c252a997f49307bb0245061625c2b415
xorblo-doitus <blabla86420@gmail.com>
2023-06-02 19:05:26 +0200

    Fix html window size bug (aigain, but cleaner)

--- src/singletons/platform_specific/html.gd
+++ src/singletons/platform_specific/html.gd
@@ -8,10 +8,24 @@ func _ready() -> void:
 	
 	add_child(preload("res://src/porting/html_ui.tscn").instantiate())
 	
-	while not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		await Wait.ms(0)
-		pass
-	get_window().mode = Window.MODE_FULLSCREEN
-	for __ in range(100):
-		await Wait.ms(20)
-		get_window().mode = Window.MODE_WINDOWED
+	# Fix the window size bug of Godot 4 (automatically readjust size)
+	JavaScriptBridge.eval("document.querySelector("#canvas").height = 0;")
+	
+#	var true_size: Vector2 = Vector2(
+#		JavaScriptBridge.eval("""document.querySelector("#canvas").width"""),
+#		JavaScriptBridge.eval("""document.querySelector("#canvas").height""")
+#	)
+#	print("True sze is ", true_size)
+#	print("window_size_is ", get_window().size)
+#	if true_size.x != get_window().size.x and true_size.y != get_window().size.y:
+#		await Wait.secondes(1)
+#		JavaScriptBridge.eval("""
+#		document.querySelector("#canvas").height = %s;
+#		""" % get_window().size.y)
+#		while not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+#			await Wait.ms(0)
+#			pass
+#		get_window().mode = Window.MODE_FULLSCREEN
+#		for __ in range(100):
+#			await Wait.ms(20)
+#			get_window().mode = Window.MODE_WINDOWED
